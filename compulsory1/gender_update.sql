c3a47b10e10a4402b03ea86e0b16756c471f9069
sabr5840 <sabr5840@stud.kea.dk>
2025-09-23 11:22:12 +0200

    Generated diff-script between InitialCreate and AddGenderToStudent (gender_update.sql)

---
+++ compulsory1/gender_update.sql
@@ -0,0 +1,8 @@
+﻿BEGIN TRANSACTION;
+ALTER TABLE "Students" ADD "Gender" TEXT NOT NULL DEFAULT '';
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20250923082349_AddGenderToStudent', '9.0.9');
+
+COMMIT;
+
