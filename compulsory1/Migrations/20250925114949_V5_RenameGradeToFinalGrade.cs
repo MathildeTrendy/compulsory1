6b1f47faa5816a8b9ad27409d4e7b3860472881a
MathildeTrendy <math38v6@stud.kea.dk>
2025-09-25 13:49:52 +0200

    feat(ef): V5 rename Grade -> FinalGrade

---
+++ compulsory1/Migrations/20250925114949_V5_RenameGradeToFinalGrade.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace compulsory1.Migrations
+{
+    /// <inheritdoc />
+    public partial class V5_RenameGradeToFinalGrade : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Grade",
+                table: "Enrollments",
+                newName: "FinalGrade");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FinalGrade",
+                table: "Enrollments",
+                newName: "Grade");
+        }
+    }
+}
